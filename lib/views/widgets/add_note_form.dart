b5b7de1de129adb99850d9647768d256083d96b1
Belkadi Sidahmed <sidahmed.belkadi0@gmail.com>
2024-04-28 15:57:08 +0100

    Create Add Note Form

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_hive_db/views/widgets/add_note_form_button.dart';
 
 class AddNoteForm extends StatelessWidget {
   const AddNoteForm({super.key});
@@ -7,6 +8,23 @@ class AddNoteForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(12.0),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(label: Text("Title")),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              minLines: 6,
+              maxLines: 10,
+              decoration: const InputDecoration(label: Text("Description")),
+            ),
+            const SizedBox(height: 36),
+            const CustomAddNoteFormElevatedButton()
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/views/widgets/add_note_form_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomAddNoteFormElevatedButton extends StatelessWidget {
+  const CustomAddNoteFormElevatedButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+        child: const Text(
+          "Create note",
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
