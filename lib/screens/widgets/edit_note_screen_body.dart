7bd6c3c6e243eeee5d85d60c9d0c9ac6fc7bd9b1
Belkadi Sidahmed <sidahmed.belkadi0@gmail.com>
2024-04-28 18:10:42 +0100

    Complete EditNote UI

--- lib/screens/widgets/edit_note_screen_body.dart
+++ lib/screens/widgets/edit_note_screen_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:notes_app_hive_db/screens/widgets/custom_app_bar.dart';
+import 'package:notes_app_hive_db/screens/widgets/custom_text_form_field.dart';
 
 class EditNoteScreenBody extends StatelessWidget {
   const EditNoteScreenBody({super.key});
@@ -12,6 +14,16 @@ class EditNoteScreenBody extends StatelessWidget {
         child: Column(
           children: [
             CustomAppBar(title: "Edit Note", icon: Icons.check),
+            Form(
+              child: Column(
+                children: [
+                  SizedBox(height: 36),
+                  CustomTextFormField(title: "Title"),
+                  SizedBox(height: 16),
+                  CustomTextFormField(title: "Description", maxLines: 6),
+                ],
+              ),
+            ),
           ],
         ),
       ),
