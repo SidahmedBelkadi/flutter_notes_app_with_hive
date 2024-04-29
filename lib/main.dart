401978ad2bf7081f18dd3aedad46bee54624359b
Belkadi Sidahmed <sidahmed.belkadi0@gmail.com>
2024-04-29 16:49:55 +0100

    Init Hive and open Notes box.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app_hive_db/core/constants/app_strings.dart';
 import 'core/theme/app_theme.dart';
 
 import 'screens/notes_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Initialize Hive And Setup Notes Box.
+  await Hive.initFlutter();
+  await Hive.openBox(kNotes);
+
   runApp(const NotesApp());
 }
 
